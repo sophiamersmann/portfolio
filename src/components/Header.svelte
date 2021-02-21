e717c80942ff1efc797907aebec0ce2ddf3a5d2d
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-02-21 10:29:53 +0100

    Reset, init svelte project

---
+++ src/components/Header.svelte
@@ -0,0 +1,11 @@
+<script>
+  import SocialMedia from './SocialMedia.svelte';
+</script>
+
+<header>
+  <div class="brand">
+    <img src="img/memoji-421x421.png" alt="Memoji" width="421" height="421" />
+    <h1>Sophia Mersmann</h1>
+  </div>
+  <SocialMedia />
+</header>
